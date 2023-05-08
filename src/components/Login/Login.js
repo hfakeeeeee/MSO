03a9e5ec63d3e2e4db5bcee1e50b993a3bbb149b
hfakeeeeee <hfake.xyz@gmail.com>
2023-05-09 00:22:00 +0700

    Finish UI for Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,7 @@
-// src/components/Login/Login.js
 import React, { useState } from "react";
 import { auth } from "../../firebase";
 import "./Login.css";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
@@ -15,49 +13,57 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-        const userCredential =  signInWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        navigate('/Lobby');
-        // Login successful, you can now perform additional actions, e.g., redirect to another page, etc.
-      } catch (error) {
-        // Handle login errors here.
-        console.error(error);
-      }
+      const userCredential = signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      navigate("/Lobby");
+      // Login successful, you can now perform additional actions, e.g., redirect to another page, etc.
+    } catch (error) {
+      // Handle login errors here.
+      console.error(error);
+    }
   };
 
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-        const userCredential =  createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        // Registration successful, you can now perform additional actions, e.g., update profile, redirect to another page, etc.
-      } catch (error) {
-        // Handle registration errors here.
-        console.error(error);
-      }
+      const userCredential = createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      // Registration successful, you can now perform additional actions, e.g., update profile, redirect to another page, etc.
+    } catch (error) {
+      // Handle registration errors here.
+      console.error(error);
+    }
   };
 
   return (
     <div className="login">
-      <h1>{isLogin ? "Login" : "Register"}</h1>
-      <form onSubmit={isLogin ? handleLogin : handleRegister}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">{isLogin ? "Login" : "Register"}</button>
-      </form>
-      <button onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? "Switch to Register" : "Switch to Login"}
-      </button>
+        <h1>LOGIN</h1>
+        <form onSubmit={isLogin ? handleLogin : handleRegister}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="button-group">
+            <button className="login-btn" type="submit">
+              Sign in
+            </button>
+            <button className="signup-btn" onClick={() => setIsLogin(!isLogin)}>
+              Sign up
+            </button>
+          </div>
+        </form>
     </div>
   );
 };
