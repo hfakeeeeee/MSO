d4e673f414d71f8ca6ade277731c0d224660723d
hfakeeeeee <hfake.xyz@gmail.com>
2023-05-08 15:38:54 +0700

    Cập nhật thêm UI cho login
    Thêm box, chỉnh màu, căn chỉnh bố cục

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,12 @@
 import React, { useState } from "react";
 import { auth } from "../../firebase";
 import "./Login.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -15,49 +18,59 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-        const userCredential =  signInWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        navigate('/Lobby');
-        // Login successful, you can now perform additional actions, e.g., redirect to another page, etc.
-      } catch (error) {
-        // Handle login errors here.
-        console.error(error);
-      }
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      navigate("/Lobby");
+      // Login successful, you can now perform additional actions, e.g., redirect to another page, etc.
+    } catch (error) {
+      // Handle login errors here.
+      console.error(error);
+    }
   };
 
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-        const userCredential =  createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        // Registration successful, you can now perform additional actions, e.g., update profile, redirect to another page, etc.
-      } catch (error) {
-        // Handle registration errors here.
-        console.error(error);
-      }
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      // Registration successful, you can now perform additional actions, e.g., update profile, redirect to another page, etc.
+    } catch (error) {
+      // Handle registration errors here.
+      console.error(error);
+    }
   };
 
   return (
     <div className="login">
-      <h1>{isLogin ? "Login" : "Register"}</h1>
-      <form onSubmit={isLogin ? handleLogin : handleRegister}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">{isLogin ? "Login" : "Register"}</button>
-      </form>
-      <button onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? "Switch to Register" : "Switch to Login"}
-      </button>
+      <div className="login-box">
+        <h1>{isLogin ? "Login" : "Register"}</h1>
+        <form onSubmit={isLogin ? handleLogin : handleRegister}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">{isLogin ? "Login" : "Register"}</button>
+        </form>
+        <div className="switch" onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? "Don't have an account? Register here" : "Already have an account? Login here"}
+        </div>
+      </div>
     </div>
   );
 };
