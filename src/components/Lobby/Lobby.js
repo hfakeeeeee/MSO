79673efc045f39388fe1c1daccffd328a96d252f
hfakeeeeee <hfake.xyz@gmail.com>
2023-05-12 15:28:10 +0700

    Fixing Lobby UI

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -15,6 +15,7 @@ const Lobby = () => {
   const [showUserInfo, setShowUserInfo] = useState(false);
   const [showCreateGameInput, setShowCreateGameInput] = useState(false);
   const [newGameName, setNewGameName] = useState('');
+  const [expandRoomList, setExpandRoomList] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -38,6 +39,26 @@ const Lobby = () => {
     };
   }, []);
 
+  const joinRoom = async (roomId = null) => {
+    if (!user) return;
+  
+    if (roomId) {
+      navigate(`/game/${roomId}`);
+      return;
+    }
+  
+    const availableRooms = rooms.filter((room) => Object.keys(room.players).length < room.maxPlayer);
+  
+    if (availableRooms.length > 0) {
+      const randomRoomIndex = Math.floor(Math.random() * availableRooms.length);
+      const randomRoom = availableRooms[randomRoomIndex];
+  
+      navigate(`/game/${randomRoom.id}`);
+    } else {
+      createRoom();
+    }
+  };
+
   const createRoom = () => {
     if (!newRoomName) return;
   
@@ -55,31 +76,24 @@ const Lobby = () => {
       status: 'waiting',
       timeForm: 'day',
       currentTurn: 0,
-      maxPlayer:16, 
-      minPlayer:8,
+      maxPlayer: 16,
+      minPlayer: 8,
     };
   
     push(roomRef, newRoom);
   
     setNewRoomName('');
+    joinRoom(); // Join the newly created room
   };
   
-
-  const joinRoom = async (roomId) => {
-    if (!user) return;
-    const roomRef = ref(database, `rooms/${roomId}`);
-    const playersRef = child(roomRef, 'players');
-    await update(playersRef, {
-      [user.uid]: {
-        displayName: user.displayName || user.email,
-        role: 0,
-        live: 'live',
-        status: 'waiting',
-      },
-    });
-    navigate(`/game/${roomId}`);
-  };
+  const joinRandomRoom = () => {
+    if (!user || rooms.length === 0) return;
   
+    const randomRoomIndex = Math.floor(Math.random() * rooms.length);
+    const randomRoom = rooms[randomRoomIndex];
+  
+    navigate(`/game/${randomRoom.id}`);
+  };
 
   const handleLogout = async () => {
     await signOut(auth);
@@ -111,12 +125,12 @@ const Lobby = () => {
       status: 'waiting',
       timeForm: 'day',
       currentTurn: 0,
-      maxPlayer:16, 
-      minPlayer:8,
+      maxPlayer: 16,
+      minPlayer: 8,
     };
   
     push(roomRef, newRoom);
-
+  
     setNewGameName('');
     setShowCreateGameInput(false);
   };
@@ -138,41 +152,43 @@ const Lobby = () => {
           <BsPower className="logout-icon" onClick={handleLogout} />
         </div>
       )}
-      <div class="button-container">
-        <button className="quick-play" onClick={createRoom}>
+      <div className = "inside-lobby">
+        <div class="button-container">
+        <button className="quick-play" onClick={() => joinRandomRoom()}>
           Quick Play
         </button>
-        <button className="instruction-button" onClick={toggleInstructions}>
-          Instructions
-        </button>
-      </div>
-      <div className="create-game-container">
-        <button className="create-game-button" onClick={toggleCreateGameInput}>
-          Create Game
-        </button>
-        {showCreateGameInput && (
-          <div className="create-game-popup">
-            <input
-              type="text"
-              placeholder="Enter game name"
-              value={newGameName}
-              onChange={(e) => setNewGameName(e.target.value)}
-            />
-            <button className="create-game-button" onClick={createGame}>
-              Create
-            </button>
-          </div>
-        )}
-      </div>
-      <div class="room-list-container">
-        <h2>Rooms:</h2>
-        <ul className="room-list">
-          {rooms.slice(-5).map((room) => (
-            <li key={room.id} onClick={() => joinRoom(room.id)}>
-              {room.name}
-            </li>
-          ))}
-        </ul>
+          <button className="instruction-button" onClick={toggleInstructions}>
+            Instructions
+          </button>
+        </div>
+        <div className="create-game-container">
+          <button className="create-game-button" onClick={toggleCreateGameInput}>
+            Create Game
+          </button>
+          {showCreateGameInput && (
+            <div className="create-game-popup">
+              <input
+                type="text"
+                placeholder="Enter game name"
+                value={newGameName}
+                onChange={(e) => setNewGameName(e.target.value)}
+              />
+              <button className="create-button" onClick={createGame}>
+                Create
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="room-list-container">
+          <h2>Lastest game room</h2>
+          <ul className="room-list">
+            {rooms.slice(-5).map((room) => (
+              <li key={room.id} onClick={() => joinRoom(room.id)}>
+                {room.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
